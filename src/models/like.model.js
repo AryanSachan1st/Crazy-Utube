1552125a091601fd0f9a95ad4895e616237d8e00
Aryan Sachan <sachanaryaniit@gmail.com>
2025-12-16 14:35:05 +0530

    add comment, like, tweet, playlist models

---
+++ src/models/like.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const likeSchema = new mongoose.Schema(
+    {
+        comment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        tweet: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tweet"
+        },
+        likedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+    },
+    {timestamps: true}
+)
+
+export const Like = mongoose.model("Like", likeSchema)
